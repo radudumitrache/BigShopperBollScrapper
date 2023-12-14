a5b14221abfcf114903c6ef831625ad449bfac53
Clarissa003 <clarissa.dobircianu@student.nhlstenden.com>
2023-12-14 10:09:17 +0100

    Scraping functions added

--- src/Scrapper/management/commands/command.py
+++ src/Scrapper/management/commands/command.py
@@ -3,13 +3,14 @@ from django.core.management.base import BaseCommand
 #import bol scraper function
 #import save_to_db
 #import new prices from sellers
+from .scrapper_functions import* 
 
 class Command(BaseCommand):
     help = "Scrape bol.com product information"
 
     def add_arguments(self, parser):
-        parser.add_argument("website", help="Input bol.com", type=str)
-        parser.add_argument("search", help="Input the search query (ean,title)", type=str)
+        parser.add_argument("--website", help="Input bol.com", type=str)
+        parser.add_argument("--search", help="Input the search query (ean,title)", type=str)
         parser.add_argument("--headless", action='store_true', help="Input the headless options")
 
     def handle(self, *args, **kwargs):
@@ -18,14 +19,22 @@ class Command(BaseCommand):
         option = kwargs["headless"]
         print(option)
         
+        product_Info = product_info(website)
+        all_sellers_info = get_all_sellers_info(website)
+        country_code = get_country_code(website)
+        product_name = get_product_name(website)
+        product_price = get_product_price(website)
+        
+        result_dict = {
+            "name" : product_name,
+            "product_price" : product_price,
+            "country_code" : country_code,
+            "product_info" : product_Info,
+            "other_sellers" : all_sellers_info,
+        }
 
-        url = scrape_bol(search, website)  # Adjust line based on bol.com scraping logic
-        
-        result = scrape_bol_com(url)  # Adjust line based on bol.com scraping logic
-        new_prices = get_new_prices_from_sellers(url, option) 
-        print(new_prices)
-        
-        save_product_info(result, url, new_prices)
+        print(result_dict)
+
 
 # Command to run 
 # python manage.py command --website=bol.com --search="your_search_query" --headless
