5ea0deeafd213c83341efd043b917203017494b4
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2024-01-12 16:15:27 +0100

    Added test for country code

---
+++ tests/test_countrycode.py
@@ -0,0 +1,9 @@
+import unittest
+from src.Scrapper.management.commands.scrapper_functions.countryCode import get_country_code
+
+class TestCountrycode(unittest.TestCase):
+    def test_expect_nl(self):
+        self.assertEqual(get_country_code("https://www.bol.com/nl/nl/p/sony-official-playstation-5-dualsense-controller/9300000007897748/"), "NL")
+
+    def test_expect_be(self):
+        self.assertEqual(get_country_code("https://www.bol.com/be/nl/p/sony-official-playstation-5-dualsense-controller/9300000007897748/"), "BE")
