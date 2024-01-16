294a148d93d692dc2b2eb8e67d80b0ccb4a7bbe7
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2024-01-16 16:02:45 +0100

    made function names consistent and added header parameter

--- src/Scrapper/management/commands/scrapper_functions/bol_other_sellers.py
+++ src/Scrapper/management/commands/scrapper_functions/bol_other_sellers.py
@@ -1,38 +1,16 @@
-from lxml import html
 import requests
-import json
-# Define the URL of the web page you want to scrape
-HEADERS = {
-    'authority': 'spoor.bol.com',
-    'method': 'GET',
-    'path': '/app/v1/config/iylvyb75XjFdC2iUCNMO18sUE4lEXmnQCPCwOozu',
-    'scheme': 'https',
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Origin': 'https://www.bol.com',
-    'Referer': 'https://www.bol.com/',
-    'Sec-Ch-Ua': '"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"Windows"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'Sec-Gpc': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 '
-                  'Safari/537.36'
-    }
-def get_sellers(siteurl) :
-    dict_sellers = {} 
-    response = requests.get(siteurl, headers=HEADERS) 
-    if response.status_code == 200:
+from lxml import html
 
+def get_sellers(siteurl, headers):
+    dict_sellers = {}
+    response = requests.get(siteurl, headers=headers)
+
+    if response.status_code == 200:
         page_content = response.content
         parsed_content = html.fromstring(page_content)
         other_sellers = parsed_content.xpath('//ul[@id="offers"]/*')
-        
-        for seller in other_sellers :
 
+        for seller in other_sellers :
             about_seller_block = seller.xpath('.//div[@data-test="about-seller-block"]')
             about_offer_block = seller.xpath('.//div[@data-test="about-offer-block"]')
             about_price_block = seller.xpath('.//div[@data-test="about-price-block"]')
@@ -47,10 +25,10 @@ def get_sellers(siteurl) :
     else:
         print(f"Request failed with status code: {response.status_code}")
         return None
-def get_all_sellers_info(url) :
-    
+
+def get_all_sellers_info(url, headers):
     # Make a request
-    response = requests.get(url, headers=HEADERS)
+    response = requests.get(url, headers=headers)
 
     # Check the status code
     if response.status_code == 200:
@@ -59,12 +37,10 @@ def get_all_sellers_info(url) :
         page_content = response.content
         parsed_content = html.fromstring(page_content)
         all_sellers_url = parsed_content.xpath('//a[@data-label="all-sellers"]/@href')
-        
-        if (len(all_sellers_url) > 0) : 
-            all_sellers_url = "https://www.bol.com" +  all_sellers_url[0]
+
+        if (len(all_sellers_url) > 0):
+            all_sellers_url = "https://www.bol.com" + all_sellers_url[0]
             return get_sellers(all_sellers_url)
-        
-        
     else:
         print(f"Request failed with status code: {response.status_code}")
         return None
---
+++ src/Scrapper/management/commands/scrapper_functions/country_code.py
@@ -0,0 +1,33 @@
+from urllib.parse import urlparse
+
+import requests
+
+def get_country_code(url_to_scrape):
+    try:
+        # Send an HTTP request to the URL
+        response = requests.get(url_to_scrape)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # split url by '/' and get second element for country code
+            parsed_url = urlparse(url_to_scrape)
+            path_components = parsed_url.path.split('/')
+
+            # Check the country code based on the path
+            if len(path_components) > 2:
+                if path_components[1] == "nl":
+                    country_code = "NL"
+                elif path_components[1] == "be":
+                    country_code = "BE"
+                else:
+                    country_code = "N/A"
+            else:
+                country_code = "N/A"
+
+            return country_code
+
+        else:
+            print(f"Error: Unable to fetch content. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"Error: {e}")
